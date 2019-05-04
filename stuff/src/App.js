3af9d7c71e98b3df151c7fc0cd902c064f21ddb9
Tony Kieling <tony.kieling@gmail.com>
2019-05-04 11:40:35 -0700

    doing some form

--- stuff/src/App.js
+++ stuff/src/App.js
@@ -2,31 +2,56 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
 
-  constructor(){
-    super()
-    this.state = {
-      message: ".",
+  state = {
+      message: "",
       count: 0,
       onAndOff: false
     }
-  }
+
+    componentDidMount() {
+      console.log("componentDidMount");
+    }
+  
+    componentDidUpdate() {
+      console.log("componentDidUpdate");
+    }
 
   changeMsg = () => {
     console.log(this.state.onAndOff + " - " + this.state.count);
-    this.setState(prevState => ({
+    this.setState({
       message: "new message",
       count: this.state.count + 1,
-      onAndOff: !prevState.onAndOff
+      onAndOff: !this.state.onAndOff
+    });
+  }
+
+  handleChange = e => {
+    this.setState({
+      message: e.target.value
+    });
+  }
+  
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState(prevState => ({
+      count: prevState.count + 1
     }));
+    console.log("handleSubmit " + this.state.count);
   }
 
   render() {
     return (
       <div>
-        <h1> { this.state.message } </h1>
-        <h2> { this.state.count } </h2>
-        <h3> { this.state.onAndOff.toString() } </h3>
+        <h1> {this.state.message} {this.state.count} </h1>
+        <h3> {this.state.onAndOff.toString()} </h3>
         <button onClick={this.changeMsg}>Do Something</button>
+
+        <form onSubmit={this.handleSubmit}>
+          <label>New Message:
+            <input type="text" value={this.state.message} onChange={this.handleChange} />
+          </label>
+          <button>Submit</button>
+        </form>
       </div>
     )
   }
