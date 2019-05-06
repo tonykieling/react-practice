7ec1ec0ddf0c9be1bd5cbf9b78227bdd0f4fb603
Tony Kieling <tony.kieling@gmail.com>
2019-05-06 15:20:10 -0700

    documenting react forms

--- stuff/src/FormY.js
+++ stuff/src/FormY.js
@@ -9,15 +9,29 @@ export default class FormY extends Component {
     }
   }
 
+  // React form
+  // # FIRST WAY: using an in inside the form
+  //  should use e.target.nameOfTheId.value
+  //  BUT, THERE IS NO VALIDATION due to dont check on handleChange
+  // # SECOND WAY: write the value directly in a variable using this.setState and retrieve from that variable
+  //  there is the necessity of a specific variable to manage this situation
+  //  and also a handleChange method to keeping track of the changes on the variable's content
+  // # THRID WAY: using Redux
+
+  // # FORTHY WAY: 
+
   handleSubmit = e => {
     e.preventDefault();
-    // console.log("inside handleSubmit, user: ", this.state.formText);
-
-    if (this.props.checkUser(this.state.formText)) {
-      alert('Alright!!')
-      return
-    }
-    alert("NOT OK!")
+    console.log("inside handleSubmit, e.target.userId: ", e.target.userId.value);
+    console.log("inside handleSubmit, e.target: ", e.target);
+    // controlled components react forms:
+    //    https://reactjs.org/docs/forms.html#controlled-components
+    // https://www.youtube.com/watch?v=3BZZg3lS2SA
+    // if (this.props.checkUser(this.state.formText)) {
+    //   alert('Alright!!')
+    //   return
+    // }
+    // alert("NOT OK!")
   }
 
   handleChange = e => {
@@ -36,8 +50,16 @@ export default class FormY extends Component {
     return (
       <div>
           <form onSubmit={this.handleSubmit}>
-            <input type="text" value={this.state.formText} onChange={this.handleChange}/>
-            <button disabled={!this.isValid()} type="submit">Submit</button>
+            <input 
+              type="text"
+              id = "userId"
+              value={this.state.formText}
+              onChange={this.handleChange}
+              placeholder="type something here"/>
+            <button
+              disabled={!this.isValid()}
+              type="submit">Submit
+            </button>
           </form>
       </div>
     )
